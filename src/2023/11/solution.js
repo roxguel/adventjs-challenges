908747eef91ce2675b437203f1b56cdf2941b8a0
Michel Varela <roxguel@gmail.com>
2023-12-12 20:12:39 +0000

    Micro optimization

--- src/2023/11/solution.js
+++ src/2023/11/solution.js
@@ -1,15 +1,16 @@
 function getIndexsForPalindrome(word) {
-  const isPalindrome = (word) => word.split("").every((x, i) => x === word[word.length - i - 1]);
-  if (isPalindrome(word)) return [];
-  
-  const wordArr = word.split("");
-  for (let i = 0; i < wordArr.length; i++) {
-      for (let j = i + 1; j < wordArr.length; j++) {
-          const newWord = [...wordArr];
-          newWord[i] = wordArr[j];
-          newWord[j] = wordArr[i];
-          if (isPalindrome(newWord.join(""))) return [i, j];
-      }
+  const length = word.length;
+  const chars = word.split("");
+
+  if (chars.join() === chars.reverse().join()) return [];
+  chars.reverse();
+
+  for (let i = 0; i < length; i++) {
+    for (let j = i + 1; j < length; j++) {
+      [chars[i], chars[j]] = [chars[j], chars[i]];
+      if (chars.every((x, i) => x === chars[length - i - 1])) return [i, j];
+      [chars[i], chars[j]] = [chars[j], chars[i]];
+    }
   }
 
   return null;
