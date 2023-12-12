243ac5c3b200e6fbd9afb738bf688b702071a6cd
Michel Varela <roxguel@gmail.com>
2023-12-12 19:31:06 +0000

    Minor optimization Challenge 10

--- src/2023/10/solution.js
+++ src/2023/10/solution.js
@@ -1,15 +1,14 @@
 function createChristmasTree(ornaments, height) {
     let result = "";
-    const SPACE = " "
+    const SPACE = " ";
     
     let k = 0;
     for (let i = 0; i < height; i++) {
         result += SPACE.repeat(height - i - 1);
         for (let j = 0; j <= i; j++) {
-            result += ornaments[k++ % ornaments.length];
-            if (j !== i) result += SPACE;
+            result += ornaments[k++ % ornaments.length] + SPACE;
         }
-        result += "\n"
+        result = result.trimEnd() + "\n"
     }
     
     return result + SPACE.repeat(height - 1) + "|\n";
